40b1d3cb4ac3ab82c42700301cbdcdd6f597d3dc
zysea23 <149136485+zysea23@users.noreply.github.com>
2024-02-07 11:07:55 -0800

    Create a window, and implement singleton MouseListener/KeyListener for LWJGL event handling

---
+++ src/main/java/jelly/MouseListener.java
@@ -0,0 +1,104 @@
+package jelly;
+
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+
+public class MouseListener {
+    private static MouseListener instance; // only singleton
+    private double scrollX, scrollY;
+    private double xPos, yPos, lastX, lastY;
+    private boolean mouseButtonPressed[] = new boolean[3]; // 3 buttons on a mouse
+    private boolean isDragging;
+
+    private MouseListener(){
+        this.scrollX = 0.0;
+        this.scrollY = 0.0;
+        this.xPos = 0.0;
+        this.yPos  = 0.0;
+        this.lastX = 0.0;
+        this.lastY = 0.0;
+    }
+
+    public static MouseListener get() {
+        if (MouseListener.instance == null) {
+            MouseListener.instance = new MouseListener();
+        }
+        return MouseListener.instance;
+    }
+
+    public static void mousePosCallback(long window, double xpos, double ypos){
+        // last will be now pos, and now pos will be a new input pos
+        get().lastX = get().xPos;
+        get().lastY = get().yPos;
+        get().xPos = xpos;
+        get().yPos = ypos;
+        // if the mouse has been moved, and it is been pressed, so it is dragging
+        get().isDragging = get().mouseButtonPressed[0] || get().mouseButtonPressed[1] || get().mouseButtonPressed[2];
+    }
+
+    // The callback function receives the mouse button, button action and modifier bits
+    // modifiers are checking if you're holding other keys and clicking mouse
+    public static void mouseButtonCallback(long window, int button, int action, int mods){
+        if(action == GLFW_PRESS){
+            if(button < get().mouseButtonPressed.length){
+                get().mouseButtonPressed[button] = true;
+            }
+        }else if(action == GLFW_RELEASE){
+            if(button < get().mouseButtonPressed.length){
+                get().mouseButtonPressed[button] = false;
+                get().isDragging = false;
+            }
+        }
+    }
+
+    public static void mouseScrollCallback(long window, double xoffset, double yoffset) {
+        get().scrollX = xoffset;
+        get().scrollY = yoffset;
+    }
+
+    public static void endFrame(){
+        get().scrollX = 0;
+        get().scrollY = 0;
+        get().lastX = get().xPos;
+        get().lastY = get().yPos;
+    }
+
+    public static float getX(){
+        return (float)get().xPos;
+    }
+
+    public static float getY(){
+        return (float)get().yPos;
+    }
+
+    public static float getDx(){
+        return (float)(get().lastX - get().xPos);
+    }
+
+    public static float getDy(){
+        return (float)(get().lastY - get().yPos);
+    }
+
+    public static float getScrollX(){
+        return (float)(get().scrollX);
+    }
+
+    public static float getScrollY(){
+        return (float)(get().scrollY);
+    }
+
+    public static boolean isDragging(){
+        return get().isDragging;
+    }
+
+    public static boolean mouseButtonDown(int button){
+        if (button < get().mouseButtonPressed.length){
+            return get().mouseButtonPressed[button];
+        }else{
+            return false;
+        }
+    }
+
+
+
+}
---
+++ src/main/java/jelly/Window.java
@@ -0,0 +1,131 @@
+package jelly;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import util.Time;
+import jelly.KeyListener;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private int width, height;
+    private String title;
+    private static Window window = null;
+    private long glfwWindow;
+
+    private float r, g, b, a;
+    private boolean fadeToblack = false;
+
+    private Window(){
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Super Mario";
+        r = 1;
+        b = 1;
+        g = 1;
+        a = 1;
+    }
+
+    public static Window get(){
+        if (Window.window == null){
+            Window.window = new Window();
+        }
+
+        return Window.window;
+    }
+
+    public void run(){
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+
+        // Free the memory once our loop ends
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public void init(){
+        //  Set up an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit()){
+            throw new IllegalStateException("Unable to initialize GLFW.");
+        }
+        // Configure glfw
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create the window
+        glfwWindow = glfwCreateWindow(this.width, this.height,this.title, NULL, NULL);
+        if (glfwWindow == NULL){
+            throw new IllegalStateException("Failed to create GLFW window.");
+        }
+
+        glfwSetCursorPosCallback(glfwWindow, jelly.MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, jelly.MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, jelly.MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
+        //  Make OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+        //  Enable v-sync
+        glfwSwapInterval(1);
+
+        //  Make the window visible
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+
+
+    }
+
+    public void loop(){
+        float beginTime = Time.getTime();
+        float endTime = Time.getTime();
+
+        while (!glfwWindowShouldClose(glfwWindow)){
+            //  Poll event
+            glfwPollEvents();
+
+            //This function clears the screen using the color specified by the last `glClearColor` call.
+            // The `GL_COLOR_BUFFER_BIT` indicates that the color buffer (the part of the framebuffer that holds the image displayed on the screen) should be cleared.
+            glClearColor(r, g, b, a);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            if(fadeToblack){
+                r = Math.max(r - 0.01f, 0);
+                b = Math.max(b - 0.01f, 0);
+                g = Math.max(g - 0.01f, 0);
+            }
+            if(KeyListener.isKeyPressed(GLFW_KEY_SPACE)){
+                fadeToblack = true;
+            }
+
+
+            glfwSwapBuffers(glfwWindow);
+
+
+            endTime = Time.getTime();
+            float dt = endTime - beginTime;
+            beginTime = endTime;
+        }
+
+    }
+}
