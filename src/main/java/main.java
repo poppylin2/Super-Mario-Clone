56a260f0f6fb0ce5d24169e7716d84303e3ffe87
poppylin2 <lin.peiyu@northeastern.edu>
2024-11-28 21:46:53 -0800

    Completed project.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,7 @@
-import java.awt.*;
-import jelly.Window;
+import jade.Window;
 
-public class main {
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Window window = Window.get();
         window.run();
     }
